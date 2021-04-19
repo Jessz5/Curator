193dd661cf23f44b7e4d531bb7776f0201fda13a
JesseCla <JesseCla@buffalo.edu>
2021-04-19 18:47:47 -0400

    Implemented Full Radio Button Functionality on UserPost.jsx

--- src/Component/UserPost.jsx
+++ src/Component/UserPost.jsx
@@ -25,20 +25,22 @@ export default class UserPost extends React.Component {
     };
 
     applyFilter = (event) => {
-        switch(event.target.value)
+        if(event.target.value == 1)
         {
-            case 1:
-                this.setState({
-                    chosenFilter: 1
-                });
-            case 2:
-                this.setState({
-                    chosenFilter: 2
-                });
-            case 3:
-                this.setState({
-                    chosenFilter: 3
-                });
+            this.setState({
+                chosenFilter: 1
+            });
+        }
+        else if (event.target.value == 2)
+        {
+            this.setState({
+                chosenFilter: 2
+            });
+        }
+        else{
+            this.setState({
+                chosenFilter: 3
+            });
         }
     }
 
@@ -49,15 +51,15 @@ export default class UserPost extends React.Component {
         <Navbar/> 
         <div id="PostHeader">
                     <label id="radioLabel">
-                    <input type="radio" name="selfPosts" value={1} checked={this.state.chosenFilter == 1} onChange={() => this.applyFilter()} /> 
+                    <input type="radio" name="selfPosts" value={1} checked={this.state.chosenFilter == 1} onChange={(event) => this.applyFilter(event)} /> 
                     My Posts
                     </label>
                     <label id="radioLabel">
-                    <input type="radio" name="friendPosts" value={2} checked={this.state.chosenFilter == 2} onChange={() => this.applyFilter()}/> 
+                    <input type="radio" name="friendPosts" value={2} checked={this.state.chosenFilter == 2} onChange={(event) => this.applyFilter(event)}/> 
                     Friends
                     </label>
                     <label id="radioLabel">
-                    <input type="radio" name="similarPosts" value={3} checked={this.state.chosenFilter == 3} onChange={() => this.applyFilter()}/> 
+                    <input type="radio" name="similarPosts" value={3} checked={this.state.chosenFilter == 3} onChange={(event) => this.applyFilter(event)}/> 
                     Similar 
                     </label>
                     </div>
