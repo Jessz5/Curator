b141e98f0395383f993d453a02a4acebd5c91546
JesseCla <JesseCla@buffalo.edu>
2021-04-19 18:02:54 -0400

    Setup Radio Buttons for Filtering on the Top of Userpost.jsx
    Formatting is under UserPost.css. Still doesn't work, need to figure out how to pass button values in.

--- src/Component/UserPost.jsx
+++ src/Component/UserPost.jsx
@@ -1,3 +1,4 @@
+import { ThumbUpSharp } from "@material-ui/icons";
 import React from "react";
 import { Link } from "react-router-dom";
 import "../UserPost.css";
@@ -7,6 +8,7 @@ export default class UserPost extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            chosenFilter: 1
         };
 
     }
@@ -22,13 +24,45 @@ export default class UserPost extends React.Component {
         });
     };
 
+    applyFilter = (event) => {
+        switch(event.target.value)
+        {
+            case 1:
+                this.setState({
+                    chosenFilter: 1
+                });
+            case 2:
+                this.setState({
+                    chosenFilter: 2
+                });
+            case 3:
+                this.setState({
+                    chosenFilter: 3
+                });
+        }
+    }
+
 
     render() {
     return (
       <div id="PostGrid">
-          <Navbar/> 
+        <Navbar/> 
+        <div id="PostHeader">
+                    <label id="radioLabel">
+                    <input type="radio" name="selfPosts" value={1} checked={this.state.chosenFilter == 1} onChange={() => this.applyFilter()} /> 
+                    My Posts
+                    </label>
+                    <label id="radioLabel">
+                    <input type="radio" name="friendPosts" value={2} checked={this.state.chosenFilter == 2} onChange={() => this.applyFilter()}/> 
+                    Friends
+                    </label>
+                    <label id="radioLabel">
+                    <input type="radio" name="similarPosts" value={3} checked={this.state.chosenFilter == 3} onChange={() => this.applyFilter()}/> 
+                    Similar 
+                    </label>
+                    </div>
         <div className="SongInfo">
-            <PostingList/>
+            <PostingList filter={this.chosenFilter}/>
         </div>
 
 
